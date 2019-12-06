e77da57840727f5ff1b707ae10dc4085cab17b38
bahrambds <bahrambds@gmail.com>
2019-12-06 11:10:59 -0500

    trying to fix oath

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,5 +1,4 @@
 const passport = require('passport')
-// const GoogleStrategy = require('passport-google-oauth20')
 const keys = require('./keys')
 const User = require('../models/user-model')
 const GoogleStrategy = require('passport-google-oauth20');
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,26 +1,38 @@
 const router = require('express').Router()
 const passport = require('passport')
 
-
 // auth login
-router.get('/login', (req, res) => {
+router.get('/login', (req, res) =>{
     res.render('login')
-})
-
-// auth logout
-router.get('/logout', (req, res) => {
+  });
+  //auth logout
+  router.get('/logout', (req,res) => {
     //handle with passport
-    res.send('loggin out')
-})
-
-
-// auth with github
-router.get('/github', passport.authenticate('github', {
+    res.send('login out');
+  })
+  //auth with google to go to the consent screen NOTE: first time it runs authenticate
+  // specifying the scope
+  router.get('/google', passport.authenticate('google', {
     scope: ['profile']
-}))
+  }));
+  // callback router for google to redirect. at this point we recieve a code in query string. use this code to respond to google what we want. 
+  router.get('/google/redirect', passport.authenticate('google'), (err, req, res, next) => {
+    // access current user
+      //custom error handler to catch any erros, such as TokenError
+    if(err.name === 'TokenError') {
+      // redirect them back to the login page
+      res.redirect('/dashboard');
+    } else {
+      // handle other errors here
+    }
+  }, (req,res) => {
+    // On success, redirect back to dashboard
+    res.send(req.user);
+  });
+  // router.get('/google/redirect', passport.authenticate(‘google’), (req, res) => {
+  //   // access current user
+  //   res.send(req.user);
+  // })
+  module.exports = router;
+  
 
-router.get('/github/redirect', passport.authenticate('github'), (req, res) => {
-    res.send('you reached the callback URI', req.user)
-})
- 
-module.exports = router
