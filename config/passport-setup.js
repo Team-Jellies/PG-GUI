0e3fe9cdaf922340f859c2199031b1eed14ff3e3
bahrambds <bahrambds@gmail.com>
2019-12-06 11:33:28 -0500

    not fixed

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -20,6 +20,8 @@ passport.use(new GoogleStrategy({
   clientID: keys.google.clientID,
   clientSecret: keys.google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
+
+    console.log('ENTERING')
     // passport callback function. save is async, so we need to wait. console.log profile here if you want to view object
     // check if user already exists in our db. findOne is async, use promise to wait for aync
     User.findOne({googleId: profile.id}).then((currentUser) => {
@@ -40,41 +42,3 @@ passport.use(new GoogleStrategy({
     })
   })
 );
-// passport.serializeUser((user, done) => {
-//     done(null, user.id)
-// })
-
-// passport.deserializeUser((id, done) => {
-//     User.findById(id).then((user) => {
-//         done(null, user.id)
-//     })
-// })
-
-
-
-// passport.use(new GoogleStrategy({
-//     // options for the google strategy
-//     callbackURL: './auth/google/redirect',
-//     clientID: keys.google.clientID,
-//     clientSecret: keys.google.clientSecret
-//     }, (accessToken, refreshToken, profile, done) => {
-//       // passport callback function. save is async, so we need to wait. console.log profile here if you want to view object
-//       // check if user already exists in our db. findOne is async, use promise to wait for aync
-//       User.findOne({googleId: profile.id}).then((currentUser) => {
-//         if(currentUser) {
-//           // already have user
-//           console.log('user is' + currentUser)
-//           done(null, currentUser)
-//         } else {
-//           // if not, create user in db
-//           new User({
-//             username: profile.displayName,
-//             googleId: profile.id
-//           }).save().then((newUser) => {
-//             console.log('new user created' + newuser)
-//             done(null, newUser)
-//           })
-//         }
-//       })
-//     })
-//   )
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -13,15 +13,19 @@ router.get('/login', (req, res) =>{
   //auth with google to go to the consent screen NOTE: first time it runs authenticate
   // specifying the scope
   router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
-  }));
-  // callback router for google to redirect. at this point we recieve a code in query string. use this code to respond to google what we want. 
-  router.get('/google/redirect', passport.authenticate('google'), (err, req, res, next) => {
+      scope: ['profile']
+    }, (req, res) => {
+        console.log(req)
+        console.log(res)
+    }));
+    // callback router for google to redirect. at this point we recieve a code in query string. use this code to respond to google what we want. 
+    router.get('/google/redirect', passport.authenticate('google'), (err, req, res, next) => {
+        console.log('?????')
     // access current user
       //custom error handler to catch any erros, such as TokenError
     if(err.name === 'TokenError') {
       // redirect them back to the login page
-      res.redirect('/dashboard');
+      res.redirect('/');
     } else {
       // handle other errors here
     }
